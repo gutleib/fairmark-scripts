315da89ca24e18fb04e44b69f96ea5622da41191
Gleb Panov <gutleib@gmail.com>
2021-06-04 17:39:01 +0300

    working on getting list by mask

---
+++ 5.1.2_KI_list_by_filter.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+api_url='https://int01.gismt.crpt.tech/api/v3/true-api'
+echo''
+
+
+curl -X GET $api_url/products/listV2?cache=string&cis=string&cisAggregationState=string&cisPackageType=string&cisStatus=string" -H "accept: */*" -H 'Authorization: Bearer
